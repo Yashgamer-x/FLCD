dd7995a48b9065a0e0482a487e4f250458a4601d
Yashgamer-x <yashdesai121212@gmail.com>
2026-05-31 10:37:07 -0400

    Updated FirstChildPreCompute to implement balanced subtree dimension calculation; introduced area-based sorting and greedy child node assignment logic for improved layout distribution.

--- src/main/java/com/yashgamerx/flcd/service/precompute/FirstChildPreCompute.java
+++ src/main/java/com/yashgamerx/flcd/service/precompute/FirstChildPreCompute.java
@@ -2,20 +2,23 @@ package com.yashgamerx.flcd.service.precompute;
 
 import com.yashgamerx.flcd.model.AbstractNode;
 
+import java.util.Comparator;
+
 public class FirstChildPreCompute implements Precomputable {
     @Override
-    public void precompute(AbstractNode node) {
-        if (isLeafNode(node)) {
-            initializeLeafDimensions(node);
+    public void precompute(AbstractNode firstChild) {
+        if (isLeafNode(firstChild)) {
+            initializeLeafDimensions(firstChild);
             return;
         }
 
         // Injects SecondChildPreCompute dependency for all children of node
-        node.getChildren().forEach(this::injectSecondChildPrecomputation);
+        firstChild.getChildren().forEach(this::injectSecondChildPrecomputation);
 
         // Invokes precompute recursively on all children
-        node.getChildren().forEach(AbstractNode::precompute);
+        firstChild.getChildren().forEach(AbstractNode::precompute);
 
+        calculateBalancedSubTreeDimensions(firstChild);
     }
 
     /// Checks if the node has no children
@@ -34,4 +37,33 @@ public class FirstChildPreCompute implements Precomputable {
         secondChildNode.setPrecomputable(new SecondChildPreCompute());
     }
 
+    private void injectLeftSecondChildComputable(AbstractNode secondChildNode) {
+        //TODO: Implement
+    }
+
+    private void injectRightSecondChildComputable(AbstractNode secondChildNode) {
+        //TODO: Implement
+    }
+
+    private double calculateArea(AbstractNode node) {
+        return node.getSubtreeHeight() * node.getSubtreeWidth();
+    }
+
+    private void calculateBalancedSubTreeDimensions(AbstractNode firstChild) {
+        var areaComparator = Comparator.comparingDouble((AbstractNode node) -> (node.getSubtreeWidth()) * (node.getSubtreeHeight()));
+        firstChild.getChildren().sort(areaComparator);
+
+        double leftAccumulatedArea = 0;
+        double rightAccumulatedArea = 0;
+
+        for (var child : firstChild.getChildren()) {
+            if (leftAccumulatedArea <= rightAccumulatedArea) {
+                injectLeftSecondChildComputable(child);
+                leftAccumulatedArea += calculateArea(child);
+            } else {
+                injectRightSecondChildComputable(child);
+                rightAccumulatedArea += calculateArea(child);
+            }
+        }
+    }
 }
