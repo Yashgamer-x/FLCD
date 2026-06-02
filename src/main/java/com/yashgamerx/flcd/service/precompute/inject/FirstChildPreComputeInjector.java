201342df344bc8b14f0dad8cb5c05762db6bddb3
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-02 15:29:28 -0400

    Updated `FirstChildPreComputeInjector` to use `Precomputable` interface; replaced `Computable` logic with `Precomputable` for improved abstraction and consistency.

--- src/main/java/com/yashgamerx/flcd/service/precompute/inject/FirstChildPreComputeInjector.java
+++ src/main/java/com/yashgamerx/flcd/service/precompute/inject/FirstChildPreComputeInjector.java
@@ -2,15 +2,16 @@ package com.yashgamerx.flcd.service.precompute.inject;
 
 import com.yashgamerx.flcd.model.AbstractNode;
 import com.yashgamerx.flcd.service.compute.FirstChildCompute;
-import com.yashgamerx.flcd.service.compute.RootifiedCompute;
+import com.yashgamerx.flcd.service.precompute.FirstChildPreCompute;
+import com.yashgamerx.flcd.service.precompute.RootifiedPreCompute;
 
 /// Injects [FirstChildCompute] to the AbstractNode
 public class FirstChildPreComputeInjector implements PrecomputeInjectable {
     @Override
     public void inject(AbstractNode firstChild) {
-        var computable = firstChild.getComputable();
-        if ((!(computable instanceof FirstChildCompute || computable instanceof RootifiedCompute))) {
-            firstChild.setComputable(new FirstChildCompute());
+        var precomputable = firstChild.getPrecomputable();
+        if ((!(precomputable instanceof FirstChildPreCompute || precomputable instanceof RootifiedPreCompute))) {
+            firstChild.setPrecomputable(new FirstChildPreCompute());
         }
     }
 }
