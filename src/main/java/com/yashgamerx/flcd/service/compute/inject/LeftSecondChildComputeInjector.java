5dd87943aceca72fd02ff6ebe3f5e7768936e611
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-02 16:14:49 -0400

    Implemented LeftSecondChildComputeInjector to conditionally assign LeftSecondChildCompute to AbstractNode while preserving existing instances and RootifiedCompute compatibility.

---
+++ src/main/java/com/yashgamerx/flcd/service/compute/inject/LeftSecondChildComputeInjector.java
@@ -0,0 +1,15 @@
+package com.yashgamerx.flcd.service.compute.inject;
+
+import com.yashgamerx.flcd.model.AbstractNode;
+import com.yashgamerx.flcd.service.compute.LeftSecondChildCompute;
+import com.yashgamerx.flcd.service.compute.RootifiedCompute;
+
+public class LeftSecondChildComputeInjector implements ComputeInjectable {
+    @Override
+    public void inject(AbstractNode abstractNode) {
+        var precomputable = abstractNode.getComputable();
+        if ((!(precomputable instanceof LeftSecondChildCompute || precomputable instanceof RootifiedCompute))) {
+            abstractNode.setComputable(new LeftSecondChildCompute());
+        }
+    }
+}
