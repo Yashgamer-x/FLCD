8e5207ae34fd249a8e0e1f267726cdccc3579b01
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-03 14:48:51 -0400

    Enhanced `VerticalSubtreeDimensionCalculator` to calculate maximum child width and refined subtree width/height logic with `RootifiedPreCompute` handling.

--- src/main/java/com/yashgamerx/flcd/service/dimension/VerticalSubtreeDimensionCalculator.java
+++ src/main/java/com/yashgamerx/flcd/service/dimension/VerticalSubtreeDimensionCalculator.java
@@ -1,10 +1,24 @@
 package com.yashgamerx.flcd.service.dimension;
 
 import com.yashgamerx.flcd.model.AbstractNode;
+import com.yashgamerx.flcd.service.precompute.RootifiedPreCompute;
 
 public class VerticalSubtreeDimensionCalculator implements NodeDimensionCalculator {
     @Override
     public void calculate(AbstractNode node) {
+        double maxChildWidth = 0;
+        double rawChildrenHeight = 0;
 
+        for (AbstractNode child : node.getChildren()) {
+            maxChildWidth = maxWidth(node, maxChildWidth);
+        }
+    }
+
+    private double maxWidth(AbstractNode node, double maxWidth) {
+        if (node.getPrecomputable() instanceof RootifiedPreCompute) {
+            return Math.max(maxWidth, node.getSubtreeHeight());
+        }
+
+        return Math.max(maxWidth, node.getSubtreeWidth());
     }
 }
