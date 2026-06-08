4af2999fef13286c7b7167eb0cff66ff01925ff9
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-08 15:30:05 -0400

    Integrated `PreComputableFactory` into `HeightChildPreComputeInjector` and refactored `SecondChildPreCompute` to initialize `injectable` with factory-based injector for improved consistency and extensibility.

--- src/main/java/com/yashgamerx/flcd/service/precompute/inject/HeightChildPreComputeInjector.java
+++ src/main/java/com/yashgamerx/flcd/service/precompute/inject/HeightChildPreComputeInjector.java
@@ -3,14 +3,23 @@ package com.yashgamerx.flcd.service.precompute.inject;
 import com.yashgamerx.flcd.model.AbstractNode;
 import com.yashgamerx.flcd.service.precompute.HeightChildPreCompute;
 import com.yashgamerx.flcd.service.precompute.RootifiedPreCompute;
+import com.yashgamerx.flcd.service.precompute.factory.PreComputableFactory;
+import com.yashgamerx.flcd.service.precompute.factory.PreComputableOption;
 
 /// Injects [HeightChildPreCompute] dependency to the node
 public class HeightChildPreComputeInjector implements PrecomputeInjectable {
+
+    private final PreComputableFactory factory;
+
+    public HeightChildPreComputeInjector(PreComputableFactory factory) {
+        this.factory = factory;
+    }
+
     @Override
     public void inject(AbstractNode abstractNode) {
         var precomputable = abstractNode.getPrecomputable();
         if ((!(precomputable instanceof HeightChildPreCompute || precomputable instanceof RootifiedPreCompute))) {
-            abstractNode.setPrecomputable(new HeightChildPreCompute());
+            abstractNode.setPrecomputable(factory.getPreComputable(PreComputableOption.HEIGHT_CHILD));
         }
     }
 }
