f7e4d014bb5e9ac9764d5ae1fabf1746ecc4865f
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-08 15:31:35 -0400

    Integrated `PreComputableFactory` into `WidthChildPreComputeInjector` to enable factory-based `Precomputable` injection for improved consistency and extensibility.

--- src/main/java/com/yashgamerx/flcd/service/precompute/inject/WidthChildPreComputeInjector.java
+++ src/main/java/com/yashgamerx/flcd/service/precompute/inject/WidthChildPreComputeInjector.java
@@ -3,14 +3,23 @@ package com.yashgamerx.flcd.service.precompute.inject;
 import com.yashgamerx.flcd.model.AbstractNode;
 import com.yashgamerx.flcd.service.precompute.RootifiedPreCompute;
 import com.yashgamerx.flcd.service.precompute.WidthChildPreCompute;
+import com.yashgamerx.flcd.service.precompute.factory.PreComputableFactory;
+import com.yashgamerx.flcd.service.precompute.factory.PreComputableOption;
 
 /// Injects [WidthChildPreCompute] dependency and then precomputes that child.
 public class WidthChildPreComputeInjector implements PrecomputeInjectable {
+
+    private final PreComputableFactory factory;
+
+    public WidthChildPreComputeInjector(PreComputableFactory factory) {
+        this.factory = factory;
+    }
+
     @Override
     public void inject(AbstractNode abstractNode) {
         var precomputable = abstractNode.getPrecomputable();
         if ((!(precomputable instanceof WidthChildPreCompute || precomputable instanceof RootifiedPreCompute))) {
-            abstractNode.setPrecomputable(new WidthChildPreCompute());
+            abstractNode.setPrecomputable(factory.getPreComputable(PreComputableOption.WIDTH_CHILD));
         }
     }
 }
