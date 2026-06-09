e681423cf21c04abbfdea50228bbefcf87c40f5c
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-09 19:15:42 -0400

    Added `PreComputeInjectorFactoryImplementation` to provide a concrete implementation for factory-based handling of `PrecomputeInjectable` injectors.

---
+++ src/main/java/com/yashgamerx/flcd/service/precompute/inject/factory/PreComputeInjectorFactoryImplementation.java
@@ -0,0 +1,10 @@
+package com.yashgamerx.flcd.service.precompute.inject.factory;
+
+import com.yashgamerx.flcd.service.precompute.inject.PrecomputeInjectable;
+
+public class PreComputeInjectorFactoryImplementation implements PreComputeInjectorFactory {
+    @Override
+    public <T extends PrecomputeInjectable> T getInjector(PreComputeInjectorOption option) {
+        return null;
+    }
+}
