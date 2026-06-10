4e17f487c36d5d57e626fae9b91c7627fb46e9b4
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-10 19:33:55 -0400

    Refactored `LeftSecondRootifiedCompute` and `RightSecondRootifiedCompute` to use `ComputeInjectorFactory` for constructor-based initialization, improving modularity and consistency.

--- src/main/java/com/yashgamerx/flcd/service/compute/RightSecondRootifiedCompute.java
+++ src/main/java/com/yashgamerx/flcd/service/compute/RightSecondRootifiedCompute.java
@@ -1,4 +1,9 @@
 package com.yashgamerx.flcd.service.compute;
 
+import com.yashgamerx.flcd.service.compute.inject.factory.ComputeInjectorFactory;
+
 public class RightSecondRootifiedCompute extends RootifiedCompute {
+    public RightSecondRootifiedCompute(ComputeInjectorFactory computeInjectorFactory) {
+        super(computeInjectorFactory);
+    }
 }
