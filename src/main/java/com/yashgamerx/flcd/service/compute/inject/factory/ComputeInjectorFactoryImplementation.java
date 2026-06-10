b1ce360e4dd33f9ed9f8e3b0252c192278508278
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-10 19:03:22 -0400

    Added caching logic to `ComputeInjectorFactoryImplementation` and extended `ComputeInjectorOption` with new injector types for improved performance and extensibility.

--- src/main/java/com/yashgamerx/flcd/service/compute/inject/factory/ComputeInjectorFactoryImplementation.java
+++ src/main/java/com/yashgamerx/flcd/service/compute/inject/factory/ComputeInjectorFactoryImplementation.java
@@ -1,10 +1,26 @@
 package com.yashgamerx.flcd.service.compute.inject.factory;
 
-import com.yashgamerx.flcd.service.compute.inject.ComputeInjectable;
+import com.yashgamerx.flcd.service.compute.inject.*;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 public class ComputeInjectorFactoryImplementation implements ComputeInjectorFactory {
+    private final Map<ComputeInjectorOption, ComputeInjectable> computeInjectorCache = new EnumMap<>(ComputeInjectorOption.class);
+
     @Override
+    @SuppressWarnings("unchecked")
     public <T extends ComputeInjectable> T getInjector(ComputeInjectorOption option) {
-        return null;
+        if (option == null) throw new IllegalArgumentException("Option cannot be null");
+
+        return (T) computeInjectorCache.computeIfAbsent(option, key -> switch (key) {
+            case FIRST_CHILD -> new FirstChildComputeInjector();
+            case LEFT_SECOND_CHILD -> new LeftSecondChildComputeInjector();
+            case RIGHT_SECOND_CHILD -> new RightSecondChildComputeInjector();
+            case LEFT_HEIGHT_CHILD -> new LeftHeightChildComputeInjector();
+            case RIGHT_HEIGHT_CHILD -> new RightHeightChildComputeInjector();
+            case LEFT_WIDTH_CHILD -> new LeftWidthChildComputeInjector();
+            case RIGHT_WIDTH_CHILD -> new RightWidthChildComputeInjector();
+        });
     }
 }
